9637128b57430aeccd6183e5c74df2721fbef9fc
fexcode <2734664632@qq.com>
2026-01-30 19:32:04 +0800

    添加分平台运行

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from src.routers import app
+import sys
 
-app.run(debug=True)
+# 如果是windows就是debug
+if sys.platform == "win32":
+    app.run(debug=True, use_reloader=False)
+else:
+    app.run(debug=False, port=11451)
